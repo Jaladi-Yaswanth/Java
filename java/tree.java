bc2b1c381e9ccf58e76ef55b61c502790e09980d
Yaswanth <134692338+Jaladi-Yaswanth@users.noreply.github.com>
2024-09-07 18:27:07 +0530

    Update tree.java

--- java/tree.java
+++ java/tree.java
@@ -1,18 +1,1 @@
-class tree{
-    class Node{
-        int data;
-        Node left,right,root;
-
-        Node(int data){
-            this.data=data;
-            left=right=root=null;
-        }
-    }
-
-    public int Insert()
-
-    public static void main(String args[]){
-      tree obj=new tree();
-      
-    }
-}
+nothing
